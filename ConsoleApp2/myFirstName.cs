96dbe1f657edc58d1dc39aa693b16718184cdc37
meganhainline <meg.hainline@gmail.com>
2018-07-18 19:56:57 -0500

    moved file

---
+++ ConsoleApp2/myFirstName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class myFirstName
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            int x;
+            int y;
+
+            x = 7;
+                y = x + 3;
+                Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+                Console.Write("type your first name:");
+                String myFirstName;
+            myFirstName = Console.ReadLine();
+
+
+            string myLastName;
+            Console.Write("type your last name:");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
+            Console.ReadLine();
+        }
+    }
+}
